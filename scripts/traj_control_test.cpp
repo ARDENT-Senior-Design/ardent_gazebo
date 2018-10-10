3e8906a85e9c1e71407663e2df3b04b3e26869ba
Nathan Boyd <nboyd2226@gmail.com>
2018-10-10 14:34:24 -0700

    Added control script for trajectory

---
+++ scripts/traj_control_test.cpp
@@ -0,0 +1,13 @@
+#include <exception>
+#include <string>
+#include <ros/ros.h>
+#include <actionlib/client/simple_action_client.h>
+#include <control_msgs/FollowJointTrajectoryAction.h>
+#include <boost/shared_ptr.hpp>
+
+
+int main()
+{
+
+    return 0;
+}
